6641da208691b90cf7c3a77a7391a039b4070f2d
Jewel Barua <jewelbarua37@gmail.com>
2021-03-17 21:04:51 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,11 @@
+// const age = 4; // if const age = 0; condition is false
+const  name = " ";
+if(name){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false"); // if anything is not defined javascript will select false
+}
+
+// falsy Values =  0, "", undefined, null, NaN
+// Truthy values = '0', " ", [], {}
